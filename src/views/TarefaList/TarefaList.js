cbb0f175833c082bcb9ed9f267ef44b95c38f014
CesarAugusto <cesaraugusto.ls@hotmail.com>
2020-04-15 19:45:42 -0300

    post tarefa

--- src/views/TarefaList/TarefaList.js
+++ src/views/TarefaList/TarefaList.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/styles';
 
 import { TarefasToolbar, TarefasTable } from './components';
 
+import axios from 'axios'
+
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3)
@@ -17,9 +19,22 @@ const TarefasList = () => {
 
   const [tarefas] = useState([]);
 
+  const salvar = (tarefa) => {
+    const URL = 'https://minhastarefas-api.herokuapp.com/tarefas'
+    axios.post(
+      URL,
+      tarefa,
+      {headers : {'x-tenant-id' : 'fulano@email.com'}
+    }).then(response => {
+      console.log(response.data)
+    }).catch(erro => {
+      console.log(erro)
+    })
+  }
+
   return (
     <div className={classes.root}>
-      <TarefasToolbar />
+      <TarefasToolbar salvar={salvar}/>
       <div className={classes.content}>
         <TarefasTable tarefas={tarefas} />
       </div>
--- src/views/TarefaList/components/TarefasToolbar/TarefasToolbar.js
+++ src/views/TarefaList/components/TarefasToolbar/TarefasToolbar.js
@@ -46,6 +46,11 @@ const TarefasToolbar = props => {
   const submit = (event) =>{
     event.preventDefault();
     console.log(`Valores: descricao - ${descricao}, categoria - ${categoria}`)
+    const tarefa = {
+      descricao : descricao,
+      categoria : categoria
+    }
+    props.salvar(tarefa)
   }
 
   return (
